5c1201462a77ec568a46c4d487b8a87baea77b1f
alvarofranciscomiranda <up201603694@webmail.fe.up.pt>
2019-03-23 17:05:26 +0000

    Classes gets and sets done

---
+++ Game.h
@@ -0,0 +1,21 @@
+//
+// Created by Teresa Ferreira on 22/03/2019.
+//
+
+#ifndef FEUP_IART_GAME_H
+#define FEUP_IART_GAME_H
+
+#include "Board.h"
+
+
+
+class Game {
+    protected:
+        
+
+    public:
+        
+};
+
+
+#endif //FEUP_IART_GAME_H

7ba6dd1daecc617c35f1e8b8c60d22774e8bbf95
Unknown <up201603811@fe.up.pt>
2019-03-25 13:57:04 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ cmake-build-debug/CMakeFiles/FEUP_IART.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Teresa/FEUP/3/IART/FEUP-IART/Board.cpp" "/Users/Teresa/FEUP/3/IART/FEUP-IART/cmake-build-debug/CMakeFiles/FEUP_IART.dir/Board.cpp.o"
+  "/Users/Teresa/FEUP/3/IART/FEUP-IART/Cell.cpp" "/Users/Teresa/FEUP/3/IART/FEUP-IART/cmake-build-debug/CMakeFiles/FEUP_IART.dir/Cell.cpp.o"
+  "/Users/Teresa/FEUP/3/IART/FEUP-IART/Piece.cpp" "/Users/Teresa/FEUP/3/IART/FEUP-IART/cmake-build-debug/CMakeFiles/FEUP_IART.dir/Piece.cpp.o"
+  "/Users/Teresa/FEUP/3/IART/FEUP-IART/main.cpp" "/Users/Teresa/FEUP/3/IART/FEUP-IART/cmake-build-debug/CMakeFiles/FEUP_IART.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/FEUP_IART.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FEUP_IART.dir/main.cpp.o"
+  "CMakeFiles/FEUP_IART.dir/Piece.cpp.o"
+  "CMakeFiles/FEUP_IART.dir/Cell.cpp.o"
+  "CMakeFiles/FEUP_IART.dir/Board.cpp.o"
+  "FEUP_IART.pdb"
+  "FEUP_IART"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FEUP_IART.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

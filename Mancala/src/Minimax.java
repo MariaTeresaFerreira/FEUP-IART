5a774b993de9370c5d9aa58dc854f990a9909de5
Unknown <up201603811@fe.up.pt>
2019-05-20 18:13:54 +0100

    construct tree maybe working?

--- Mancala/src/Minimax.java
+++ Mancala/src/Minimax.java
@@ -1,9 +1,27 @@
+import java.util.List;
 import java.util.Vector;
 
 public abstract class Minimax {
     private Tree tree;
 
+    public void constructTree(Board b){
+        Node root = new Node(b);
+        tree = new Tree(root);
+        constructTree(root);
+    }
 
+    protected void constructTree(Node parent){
+        Vector<Integer> possiblePlays = parent.board.getValidMoves();
+        int activePlayer = parent.board.getActivePlayer();
+        possiblePlays.forEach(n -> {
+            Board newBoard = parent.board;
+            newBoard.move(activePlayer, n);
+            Node newNode = new Node(newBoard, activePlayer, n);
+            parent.addChild(newNode);
+            if(newBoard.getValidMoves().size() > 0)
+                constructTree(newNode);
+        });
 
+    }
 
 }
